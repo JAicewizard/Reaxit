845b2030a42c793c27dc0809f7200263944d08c3
jaap aarts <jaap.aarts1@gmail.com>
2022-10-12 18:54:45 +0200

    Reduce even further

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:reaxit/ui/screens/members_screen.dart';
 
-Future<void> main() async {
+void main() {
   runApp(const ThaliApp());
 }
 
@@ -10,7 +9,40 @@ class ThaliApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MembersScreen(),
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: GridView.builder(
+        padding: EdgeInsets.zero,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 3,
+          mainAxisSpacing: 8,
+          crossAxisSpacing: 8,
+        ),
+        itemCount: 20,
+        itemBuilder: (context, index) => MemberTile(),
+      ),
+    );
+  }
+}
+
+class MemberTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      clipBehavior: Clip.antiAlias,
+      color: Colors.white,
+      // shape: const RoundedRectangleBorder(
+      // /*borderRadius: BorderRadius.all(Radius.circular(0.1))*/),
+      child: Builder(
+        builder: (BuildContext context) {
+          return const Material(
+            color: Colors.transparent,
+          );
+        },
+      ),
     );
   }
 }
