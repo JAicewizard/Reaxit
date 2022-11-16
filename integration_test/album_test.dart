d78434b6adc02207024ecb6dcceda00d75d008f8
jaap aarts <jaap.aarts1@gmail.com>
2022-11-16 20:48:27 +0100

    reduce app

---
+++ integration_test/album_test.dart
@@ -0,0 +1,40 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:reaxit/main.dart' as app;
+
+const imagelink1 =
+    'https://upload.wikimedia.org/wikipedia/commons/7/7b/Image_in_Glossographia.png';
+
+WidgetTesterCallback getTestMethod() {
+  return (tester) async {
+    // Start app
+    app.main();
+    await tester.pumpAndSettle();
+    await Future.delayed(const Duration(seconds: 5));
+    await tester.pumpAndSettle();
+
+    expect(
+      find.image(
+        const NetworkImage(imagelink1),
+      ),
+      findsOneWidget,
+    );
+  };
+}
+
+void main() {
+  final _ = IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group(
+    'Album',
+    () {
+      testWidgets(
+        'able to load an album',
+        getTestMethod(),
+      );
+    },
+  );
+}
